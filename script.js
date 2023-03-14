0836ae95d1762ef9760667cf62e33507c4cbdb05
Victor Enweoru <venweoru@gmail.com>
2023-03-14 20:48:03 +0100

    Completed scripting

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 const initializaSection = document.querySelector(".start_game");
 const gamePlaySection = document.querySelector(".play_game");
 const cells = Array.from(document.querySelectorAll(".cells"));
+const overlay = document.querySelector(".overlay_bg");
+const endGameBox = document.querySelector(".end_game");
+const xScoreBox = document.getElementById("x_score");
+const oScoreBox = document.getElementById("o_score");
+const tieScoreBox = document.getElementById("tie_score");
+
+
+
+
 
 
 class Player {
@@ -12,6 +21,10 @@ class Player {
 }
 let spaces = Array(9).fill(null);
 let currentPlayer; 
+let xScore = 0;
+let oScore = 0;
+let tieScore = 0;
+
 
 const startGame = () => {
     initializaSection.style.display = "none";
@@ -65,8 +78,12 @@ const initialiseGame = (() => {
     const _playCpu = document.getElementById("cpu");
     const _playHuman = document.getElementById("player");
 
-    _playHuman.onclick = () => {
-        startGame();
+    _playHuman.onclick = e => {
+        buttonTransition(e.target);
+        setTimeout(() => {
+            startGame();
+            
+        }, 350);
     }
 
     _playCpu.onclick = () => {
@@ -96,37 +113,55 @@ const gameDisplay = () => {
 
     const showWinner = (winningBlocks) => {
         const winningMark = spaces[winningBlocks[1]];
+        if (typeof winningBlocks == "object"){
+            winningBlocks.map( block => {
+                if (spaces[block] == "x"){
+                    cells[block].classList.add("win_x");
+                } else {
+                    cells[block].classList.add("win_o");
+                }
+            });
+            setTimeout(() => {
+                congratulateWinner(winningMark);
+            }, 200);
+        } else {
+            setTimeout(() => {
+                congratulateWinner(winningMark);
+            }, 200);
+        }
 
-        // cells.forEach(cell => cell.removeEventListener("click", ))
-        winningBlocks.map( block => {
-            if (spaces[block] == "x"){
-                cells[block].classList.add("win_x");
-                console.log(cells[block])
-            } else {
-                cells[block].classList.add("win_o");
-                console.log(cells[block])
-            }
-        });
-
-        setTimeout(() => {
-            congratulateWinner(winningMark);
-        }, 500);
     }
 
-    return {setPlayerTurn, hoverPic, showWinner}
+return {setPlayerTurn, hoverPic, showWinner}
 }
 
-    const congratulateWinner = (winningMark) => {
-        const congrats = document.getElementById("congrats");
-        const winner = document.getElementById("winner");
-        const endGameBox = document.querySelector(".end_game");
-        const overlay = document.querySelector(".overlay_bg");
+const congratulateWinner = (winningMark) => {
+    const _congrats = document.getElementById("congrats");
+    const _winner = document.getElementById("winner");
+    const _winnerStatus = document.getElementById("winner_status");
 
-        congrats.textContent = "CONGRATULATIONS!";
-        winner.setAttribute("scr", `./assets/icon-${winningMark}.svg`);
-        endGameBox.style.display = "grid";
-        overlay.style.display = "block";
+    if (winningMark == "x" || winningMark == "o"){
+        _congrats.textContent = "CONGRATULATIONS!";
+        _winner.src = `./assets/icon-${winningMark}.svg`;
+        _winnerStatus.textContent = "TAKES THE ROUND";
+        winningMark == "x" ? xScore++ : oScore++;
+    } else {
+        _congrats.textContent = "";
+        _winner.src = "";
+        _winnerStatus.textContent = "IT'S A TIE";
+        tieScore++;
     }
+    xScoreBox.textContent = xScore;
+    oScoreBox.textContent = oScore;
+    tieScoreBox.textContent = tieScore;
+    endGameBox.style.display = "grid";
+    overlay.style.display = "block";
+    // updateScores();
+}
+
+const updateScores = () => {
+
+}
 
 const checkWinner = () => {
     const _winningCombinations = [
@@ -154,24 +189,129 @@ const checkWinner = () => {
 return {playerHasWon}
 }
 
-const cellClick = (() => {
-    cells.forEach(cell => {
-        cell.addEventListener("click", e => {
-            const id = e.target.id;
-            const target = e.target;
-            // console.log(id)
-            if (!spaces[id]) {
-                spaces[id] = currentPlayer;
-                target.setAttribute("field-state", currentPlayer);
+const playBtnClick = (e) => {
+    const id = e.target.id;
+    const target = e.target;
+    // console.log(id)
+    if (!spaces[id]) {
+        spaces[id] = currentPlayer;
+        target.setAttribute("field-state", currentPlayer);
 
-                currentPlayer = currentPlayer == startGame().player1.mark?startGame().player2.mark:startGame().player1.mark;
-                gameDisplay().hoverPic(currentPlayer);
-                gameDisplay().setPlayerTurn(currentPlayer);
+        currentPlayer = currentPlayer == startGame().player1.mark?startGame().player2.mark:startGame().player1.mark;
+        gameDisplay().hoverPic(currentPlayer);
+        gameDisplay().setPlayerTurn(currentPlayer);
+    }
+    if (checkWinner().playerHasWon() !== false){
+        let winningBlocks = checkWinner().playerHasWon();
+        gameDisplay().showWinner(winningBlocks);
+    }
+    if (spaces.every(box => box != null) && checkWinner().playerHasWon() == false) {
+        gameDisplay().showWinner("tie");
+    }
+        
+}
+
+const buttonFunc = () => {
+    const _reloadGameBox = document.querySelector(".reload_game");
+
+    const refreshGame = e => {
+        buttonTransition(e.target);
+        setTimeout(() => {
+            overlay.style.display = "block";
+            _reloadGameBox.style.display = "block";
+        }, 350);
+    }
+
+    const continueGame = e => {
+        buttonTransition(e.target);
+        setTimeout(() => {
+            overlay.style.display = "none";
+            _reloadGameBox.style.display = "none";
+        }, 350);
+    }
+
+    const restartGame = e => {
+        buttonTransition(e.target);
+        setTimeout(() => {
+            overlay.style.display = "none";
+            _reloadGameBox.style.display = "none";
+            rebootGame();
+            startGame();
+        }, 350);
+    }
+
+    const nextRound = e => {
+        buttonTransition(e.target);
+        setTimeout(() => {
+            overlay.style.display = "none";
+            endGameBox.style.display = "none";
+            spaces = Array(9).fill(null);
+            for (const cell of cells){
+                cell.setAttribute("field-state", "");
             }
-            if (checkWinner().playerHasWon() !== false){
-                let winningBlocks = checkWinner().playerHasWon();
-                gameDisplay().showWinner(winningBlocks);
+            startGame();
+            for (const cell of cells){
+                cell.className = "cells";
             }
-        })
+        }, 350);
+    }
+
+    const quitGame = e => {
+        buttonTransition(e.target);
+        setTimeout(() => {
+            gamePlaySection.style.display = "none";
+            endGameBox.style.display = "none";
+            overlay.style.display = "none";
+            initializaSection.style.display = "block";
+            rebootGame()
+        }, 350);
+    }
+
+    return {refreshGame, continueGame, restartGame, nextRound, quitGame}
+}
+
+const rebootGame = () => {
+    spaces = Array(9).fill(null);
+    for (const cell of cells){
+        cell.setAttribute("field-state", "");
+    }
+    xScore = 0;
+    oScore = 0;
+    tieScore = 0;
+    xScoreBox.textContent = xScore;
+    oScoreBox.textContent = oScore;
+    tieScoreBox.textContent = tieScore;
+    for (const cell of cells){
+        cell.className = "cells";
+    }
+}
+
+
+const actionClicks = (() => {
+    const _quitBtn = document.getElementById("quit");
+    const _nextRoundBtn = document.getElementById("next_round");
+    const _cancelBtn = document.getElementById("cancel");
+    const _restartBtn = document.getElementById("restart");
+    const _refreshBtn = document.getElementById("refresh");
+
+    cells.forEach(cell => {
+        cell.addEventListener("click", e => playBtnClick(e))
     })
+
+    _quitBtn.onclick = e => buttonFunc().quitGame(e);
+    _nextRoundBtn.onclick = e => buttonFunc().nextRound(e);
+    _cancelBtn.onclick = e => buttonFunc().continueGame(e);
+    _restartBtn.onclick = e => buttonFunc().restartGame(e);
+    _refreshBtn.onclick = e => buttonFunc().refreshGame(e);
+
 })();
+
+
+// BUTTONS CLICK TRANSITION
+const buttonTransition = (button) => {
+    button.classList.add("click");
+    setTimeout(() => {
+        button.classList.remove("click");
+        
+    }, 300);
+}
